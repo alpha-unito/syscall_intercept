97506866c381e56ee0950da624563d07a230f10a
Ottavio Monticelli <stratovarius1998@gmail.com>
2025-07-15 18:32:38 +0200

    optional inclusion of system calls not defined on RISCV

--- test/syscall_format.c
+++ test/syscall_format.c
@@ -118,7 +118,7 @@
 #ifdef SYS_arch_prctl
 	#include <asm/prctl.h>
 #endif
-#ifdef SYS_iopl || SYS_ioperm
+#if defined(SYS_iopl) || defined(SYS_ioperm)
 	#include <sys/io.h>
 #endif
 
@@ -287,6 +287,7 @@ main(int argc, char **argv)
 	pwritev(1, NULL, 4, 0x1000);
 
 	/* open, close */
+#ifdef SYS_open
 	syscall(SYS_open, input[0], O_CREAT | O_RDWR | O_SYNC, 0321);
 	syscall(SYS_open, input[0], 0, 0321);
 	syscall(SYS_open, NULL, all_o_flags, 0777);
@@ -294,6 +295,7 @@ main(int argc, char **argv)
 	syscall(SYS_open, input[1], O_RDWR | O_NONBLOCK, 0111);
 	syscall(SYS_open, input[1], 0);
 	syscall(SYS_open, NULL, 0);
+#endif
 	openat(AT_FDCWD, input[0], O_CREAT | O_RDWR | O_SYNC, 0321);
 	openat(AT_FDCWD, input[0], 0, 0321);
 	openat(AT_FDCWD, NULL, all_o_flags, 0777);
@@ -479,7 +481,9 @@ main(int argc, char **argv)
 	truncate(input[0], 4);
 	ftruncate(3, 3);
 
+#ifdef SYS_getdents
 	syscall(SYS_getdents, 4, p0, 1);
+#endif
 	syscall(SYS_getdents64, 4, p0, 1);
 
 	setup_buffers();
@@ -493,15 +497,21 @@ main(int argc, char **argv)
 	renameat(AT_FDCWD, input[0], 7, input[1]);
 	renameat(9, input[0], AT_FDCWD, input[1]);
 
+#ifdef SYS_mkdir
 	syscall(SYS_mkdir, input[0], 0644);
+#endif
 	syscall(SYS_mkdirat, AT_FDCWD, input[0], 0644);
 	mkdirat(33, input[0], 0644);
 	mkdirat(33, NULL, 0555);
+#ifdef SYS_rmdir
 	syscall(SYS_rmdir, input[0]);
 	syscall(SYS_rmdir, NULL);
+#endif
 
 	/* libc implementations might translate creat to open with O_CREAT */
+#ifdef SYS_creat
 	syscall(SYS_creat, input[0], 0644);
+#endif
 
 	link(input[0], input[1]);
 	linkat(1, input[0], 2, input[1], 0);
@@ -597,7 +607,9 @@ main(int argc, char **argv)
 	mknodat(1, input[0], 1, 2);
 	mknodat(AT_FDCWD, input[0], 1, 2);
 
+#ifdef SYS_ustat
 	syscall(SYS_ustat, 2, p0);
+#endif
 
 	statfs(input[0], p0);
 	fstatfs(4, p0);
@@ -616,12 +628,16 @@ main(int argc, char **argv)
 
 	vhangup();
 
+#ifdef SYS_modify_ldt
 	syscall(SYS_modify_ldt, 1, p0, 1);
+#endif
 
 	setup_buffers();
 	syscall(SYS_pivot_root, input[0], buffer[0]);
 
+#ifdef SYS__sysctl
 	syscall(SYS__sysctl, p0);
+#endif
 
 	prctl(PR_CAPBSET_DROP, 1, 2, 3, 4);
 #ifdef SYS_arch_prctl
@@ -682,12 +698,18 @@ main(int argc, char **argv)
 	syscall(SYS_tkill, 44, SIGSTOP);
 	syscall(SYS_tgkill, 44, 55, SIGSTOP);
 
+#ifdef SYS_time
 	syscall(SYS_time, p0);
+#endif
 
 	syscall(SYS_futex, p0, FUTEX_WAKE, 7L, p0, p1, 1L);
 
+#ifdef SYS_set_thread_area
 	syscall(SYS_set_thread_area, p0);
+#endif
+#ifdef SYS_get_thread_area
 	syscall(SYS_get_thread_area, p0);
+#endif
 
 	syscall(SYS_io_setup, 1, p0);
 	syscall(SYS_io_destroy, 77);
@@ -700,7 +722,9 @@ main(int argc, char **argv)
 	epoll_create(7);
 	epoll_create1(0);
 	epoll_create1(EPOLL_CLOEXEC);
+#ifdef SYS_epoll_wait
 	syscall(SYS_epoll_wait, 2L, p0, 4L, 5L);
+#endif
 	syscall(SYS_epoll_pwait, 2L, p0, 4L, 5L, p1, 6L);
 	epoll_ctl(2L, 3L, 4L, p0);
 
@@ -769,14 +793,18 @@ main(int argc, char **argv)
 	sync_file_range(2, 3, 4, 0);
 	sync_file_range(2, 3, 4, SYNC_FILE_RANGE_WAIT_BEFORE);
 
+#ifdef SYS_signalfd
 	syscall(SYS_signalfd, 1, p0, 12);
+#endif
 	syscall(SYS_signalfd4, 1, p0, 13, SFD_NONBLOCK);
 
 	timerfd_create(CLOCK_REALTIME, TFD_CLOEXEC);
 	timerfd_settime(1, TFD_TIMER_ABSTIME, p0, p1);
 	timerfd_gettime(2, p0);
 
+#ifdef SYS_eventfd
 	syscall(SYS_eventfd, 45);
+#endif
 	syscall(SYS_eventfd2, 47, EFD_SEMAPHORE);
 
 	fallocate(1, FALLOC_FL_PUNCH_HOLE, 3, 4);
